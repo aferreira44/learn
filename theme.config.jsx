b3d0a41af4335c01c9bab77136964cffcb17c54a
André Ferreira <hello@andreferreira.me>
2023-05-25 11:17:25 +0100

    Change project link

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
   logo: <span>Learn with Me</span>,
   project: {
-    link: 'https://github.com/aferreira44/cryptography101'
+    link: 'https://github.com/aferreira44/learn'
   }
   // ...
 }
